789fe7909111679f0262ca6543956fcc55cc4f37
LTH4823 <peter4823zx@naver.com>
2022-06-15 07:50:03 +0900

    update comment mapper

--- src/main/java/org/zerock/oscatest/mapper/CommentMapper.java
+++ src/main/java/org/zerock/oscatest/mapper/CommentMapper.java
@@ -1,5 +1,11 @@
 package org.zerock.oscatest.mapper;
 
+import org.apache.ibatis.annotations.Param;
+import org.zerock.oscatest.domain.Comment;
+import org.zerock.oscatest.dto.ListDTO;
+
+import java.util.List;
+
 public interface CommentMapper {
 
 
